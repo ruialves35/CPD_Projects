6711b687332db10e2ce7a9601d73a46b0b17f956
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-20 12:28:39 +0100

    Added udp test

--- Dynamo/src/client/Client.java
+++ Dynamo/src/client/Client.java
@@ -2,10 +2,17 @@ package client;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
+        Client.udpTest();
+    }
+
+    private static void tcpTest() {
         try {
             Socket socket = new Socket("localhost", 5500);
             DataOutputStream stream = new DataOutputStream(socket.getOutputStream());
@@ -15,6 +22,17 @@ public class Client {
             stream.flush();
             stream.close();
             socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void udpTest() {
+        try {
+            DatagramSocket socket = new DatagramSocket(5502);
+            String message = "ola udp";
+            DatagramPacket packet = new DatagramPacket(message.getBytes(), message.getBytes().length, InetAddress.getLocalHost(), 5501);
+            socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Dynamo/src/server/Server.java
+++ Dynamo/src/server/Server.java
@@ -6,6 +6,10 @@ import java.net.*;
 
 public class Server {
     public static void main(String[] args) {
+        Server.udpTest();
+    }
+
+    private static void tcpTest() {
         try {
             ServerSocket serverSocket = new ServerSocket(5500);
             Socket socket = serverSocket.accept();
@@ -14,6 +18,19 @@ public class Server {
             String message = stream.readUTF();
             System.out.println(message);
             serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void udpTest() {
+        try {
+            byte[] buffer = new byte[100];
+            DatagramSocket socket = new DatagramSocket(5501);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            String message = new String(buffer);
+            System.out.println(message);
         } catch (IOException e) {
             e.printStackTrace();
         }
