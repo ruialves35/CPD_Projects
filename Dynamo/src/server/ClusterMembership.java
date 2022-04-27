7e5921e40e9a5adcbf310d995420629b5d03f655
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-27 12:07:01 +0100

    Basic project structure

---
+++ Dynamo/src/server/ClusterMembership.java
@@ -0,0 +1,6 @@
+package server;
+
+public interface ClusterMembership {
+    void join();
+    void leave();
+}
---
+++ Dynamo/src/server/Store.java
@@ -0,0 +1,7 @@
+package server;
+
+public class Store {
+    public static void main(String[] args) {
+
+    }
+}
