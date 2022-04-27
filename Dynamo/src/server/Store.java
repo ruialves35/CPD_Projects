9e6fefe152bc7dab35d69e394b8e1389c1540a2e
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-27 15:33:49 +0100

    Collecting input args from Store

--- Dynamo/src/server/Store.java
+++ Dynamo/src/server/Store.java
@@ -2,6 +2,15 @@ package server;
 
 public class Store {
     public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("Wrong number of arguments. Please invoke the program as:");
+            System.out.println("java Store <IP_mcast_addr> <IP_mcast_port> <node_id> <Store_port>");
+            System.exit(1);
+        }
 
+        final String multicastIPAddr = args[0];
+        final String multicastIPPort = args[1];
+        final String nodeId = args[2];
+        final String storePort = args[3];
     }
 }
