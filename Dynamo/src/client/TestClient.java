c85a37c555d50f9739f2ddf7ab6624da4c7064a8
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-05-19 09:53:33 +0100

    Created test client for put and delete operations

--- Dynamo/src/client/TestClient.java
+++ Dynamo/src/client/TestClient.java
@@ -1,5 +1,16 @@
 package client;
 
+import common.Message;
+import common.Sender;
+import common.Utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TestClient {
     public static void main(String[] args) {
         if (args.length < 2 || args.length > 3) {
@@ -22,5 +33,53 @@ public class TestClient {
             System.out.println("The " + operation + " requires a key to be provided");
             System.exit(1);
         }
+
+        TestClient client = new TestClient();
+
+        if (operation.equals("join") || operation.equals("leave"))
+            client.membershipOperation(nodeAP, operation);
+        else
+            client.keyValueOperation(nodeAP, operation, operand);
+    }
+
+    private void keyValueOperation(String nodeAP, String operation, String operand) {
+        String[] nodeInfo = nodeAP.split(":", 2);
+        String nodeIP = nodeInfo[0];
+        int nodePort = Integer.parseInt(nodeInfo[1]);
+
+        String key;
+        byte[] file = null;
+
+        if (operation.equals("put")) {
+            try {
+                Path path = Paths.get(operand);
+                file = Files.readAllBytes(path);
+                key = Utils.generateKey(file);
+                System.out.println("Key=" + key);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            key = operand;
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            out.write(key.getBytes(StandardCharsets.UTF_8));
+            if (file != null) {
+                out.write("\r\n".getBytes(StandardCharsets.UTF_8));
+                out.write(file);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] body = out.toByteArray();
+
+        Message msg = new Message("REQ", operation, body);
+        Sender.sendTCPMessage(msg.toBytes(), nodeIP, nodePort);
+    }
+
+    private void membershipOperation(String nodeAP, String operation) {
+
     }
 }
